3e93a29beee3fa1b637902a2df215759eedfc5fd
testrain <hsyjonny@gmail.com>
2020-11-10 14:35:35 +0800

    update build environment script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install npm
+sudo apt-get -y install npm
 sudo npm -g install npm
 sudo npm -g install pm2
 sudo npm -g install n
-n 10.8.0
-sudo apt-get install build-essential
-sudo apt-get install libudev-dev
-sudo apt-get install postgresql postgresql-contrib
+n 14.15.0
+sudo apt-get -y install build-essential
+sudo apt-get -y install libudev-dev
+sudo apt-get -y install postgresql postgresql-contrib
 cd sdk && npm install && cd ../bnbridge && npm install
