519ce4efc0db13a21a39a12028477b9c181a73ad
testrain <hsyjonny@gmail.com>
2020-12-01 17:11:27 +0800

    update prod config

--- bnbridge/src/config/example.config.js
+++ bnbridge/src/config/example.config.js
@@ -1,8 +1,8 @@
 const config = {
   apiUrl: "https://swap.dos.network",
   apiToken: "ZTgwMTY1NjkzZjAyOTk1N2VjNDQ4MjBhNGRiODJiMGI1NjI5YjM2YjJkNjc1YjVhYjE0YmEwNTBhMDFiNDk3ZDpmYmM3MWMyOTRmOWE4N2VlM2QzMmVkZDVkNjExNTE4MTFlNDRmNzc0NDgzNzY4OWVmYWRkYmJiOWY3NjgxYzA5",
-  explorerURL: "https://explorer.binance.org/tx/",
-  etherscanURL: "https://etherscan.io/tx/",
+  explorerURL: "https://explorer.binance.org/",
+  etherscanURL: "https://etherscan.io/",
   bnbAddressLength: 42,
   erc20addressLength: 42,
 };
